9d375abf15edb2cbc7e3a222dfa46fdea865605f
kprzybyla <krzprzybyla@gmail.com>
2019-11-16 14:20:48 +0100

    Additional stuff in Main class for feature2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
-	System.out.println("Feature branch!");
+	System.out.println("Feature branch is awesome!");
+        System.out.println("Foo Boo Bar");
     }
 }
