ab15a4703921118f51b6c1c104944ef9e0a0ddd2
kprzybyla <krzprzybyla@gmail.com>
2019-11-16 14:18:53 +0100

    Additional stuff in Main class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
+        Date date = new Date();
         System.out.println("Hello Git!");
-	System.out.println("Feature branch!");
+	    System.out.println("Feature branch #2!"); //conflict here!!
+        System.out.println(date.getTime());
     }
 }
